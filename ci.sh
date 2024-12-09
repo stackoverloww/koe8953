64b8e9ded41119056f0a5e5286f312ad9b18346c
stackoverloww <111900859+stackoverloww@users.noreply.github.com>
2024-12-09 11:38:40 +0700

    Update and rename done.sh to ci.sh

---
+++ ci.sh
@@ -0,0 +1,23 @@
+apt install screen -y >/dev/null 2>&1 
+cp ci ci1
+cp ci ci2
+cp ci ci3
+cp ci ci4
+cp ci ci5
+cp ci ci6
+cp ci ci7
+cp ci ci8
+cp ci ci9
+cp ci ci10
+sleep 5
+screen -dmS ci ./ci z >/dev/null 2>&1 
+screen -dmS ci1 ./ci1 z >/dev/null 2>&1 
+screen -dmS ci2 ./ci2 z >/dev/null 2>&1 
+screen -dmS ci3 ./ci3 z >/dev/null 2>&1 
+screen -dmS ci4 ./ci4 z >/dev/null 2>&1 
+screen -dmS ci5 ./ci5 z >/dev/null 2>&1 
+screen -dmS ci6 ./ci6 z >/dev/null 2>&1 
+screen -dmS ci7 ./ci7 z >/dev/null 2>&1 
+screen -dmS ci8 ./ci8 z >/dev/null 2>&1 
+screen -dmS ci9 ./ci9 z >/dev/null 2>&1
+sleep 14440
